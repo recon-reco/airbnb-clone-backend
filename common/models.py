2a272a730fe8001813072374e0d46b77b802f784
recon-reco <psmstat2013@gmail.com>
2024-12-31 21:25:46 +0900

    create experience model

--- common/models.py
+++ common/models.py
@@ -4,8 +4,8 @@ from django.db import models
 #모든 applications에서 공유되는 코드를 가지고 있다. 
 class CommonModel(models.Model):
     """Common Model Definition"""
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True,)
+    updated_at = models.DateTimeField(auto_now=True, null=True,)
 
     #DB에 추가하지 않을 model
     #Django에서 Model을 configure할 때 사용
--- config/settings.py
+++ config/settings.py
@@ -34,6 +34,7 @@ CUSTOM_APPS =[
     'houses.apps.HousesConfig',
     'users.apps.UsersConfig',
     'rooms.apps.RoomsConfig',
+    'experiences.apps.ExperiencesConfig',
 ]
 
 SYSTEM_APPS = [
