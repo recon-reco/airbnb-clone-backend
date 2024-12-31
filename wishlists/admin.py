3562e52c3dd0d7648d5fd6cc0a9236d48e3c2857
recon-reco <psmstat2013@gmail.com>
2024-12-31 22:47:47 +0900

    fix

--- wishlists/admin.py
+++ wishlists/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import WishList
+from .models import Wishlist
 
-@admin.register(WishList)
+@admin.register(Wishlist)
 class WishlistAdmin(admin.ModelAdmin):
     list_display = (
         "name",
